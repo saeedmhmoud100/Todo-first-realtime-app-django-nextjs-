3e20b1514812feffb8460e3d8629a07cf28b6356
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-07-02 22:17:39 +0300

    create add new task page design

--- frontend/src/components/Item.tsx
+++ frontend/src/components/Item.tsx
@@ -4,15 +4,15 @@ import updateIcon from '../icons/pen-to-square-regular.svg';
 export default function Item() {
     return (
         <div className="item flex justify-between px-8 py-2 border-b border-b-gray-500">
-            <div className="task-info flex">
-            <input type="checkbox" />
+            <div className="task-info flex items-center">
+            <input type="checkbox" className="w-4 h-4"/>
             <div className="text ml-4">text</div>
 
             </div>
             <div className="icons flex">
 
-                <img src={deleteIcon.src} className="w-5 h-5 mr-4"/>
-                <img src={updateIcon.src} className="w-5 h-5" />
+                <img src={deleteIcon.src} className="w-5 h-5 mr-4 cursor-pointer"/>
+                <img src={updateIcon.src} className="w-5 h-5 cursor-pointer" />
             </div>
 
         </div>
