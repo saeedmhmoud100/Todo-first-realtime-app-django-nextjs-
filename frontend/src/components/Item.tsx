d713c339b7392dea0c8ecc70676080a95fbacb6f
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-07-02 23:09:02 +0300

    create checked tasks design

--- frontend/src/components/Item.tsx
+++ frontend/src/components/Item.tsx
@@ -1,13 +1,21 @@
+"use client";
 import deleteIcon from '../icons/trash-can-regular.svg';
 import updateIcon from '../icons/pen-to-square-regular.svg';
 import Link from "next/link";
+import {useState} from "react";
 
 export default function Item({id}:{id: number}) {
+    const [done, setDone] = useState<boolean>(false);
+    const handleCheckChange : (e: React.ChangeEvent<HTMLInputElement>) => void = (e) => {
+        setDone(n => !n)
+    }
+
+
     return (
         <div className="item flex justify-between px-8 py-2 border-b border-b-gray-500">
             <div className="task-info flex items-center">
-            <input type="checkbox" className="w-4 h-4"/>
-            <div className="text ml-4">text</div>
+            <input type="checkbox" className={`w-4 h-4`} onClick={e=> handleCheckChange(e)}/>
+            <div className={`item-text text ml-4`}>text <span className='line' style={done? {width:"336px"} : {}}></span></div>
 
             </div>
             <div className="icons flex">
