eb8b8b6f17a80b5a0272c83f15ccf4e92c879a9d
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-07-04 20:46:20 +0300

    backend: create todo model

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo (models.Model):
+    content = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
